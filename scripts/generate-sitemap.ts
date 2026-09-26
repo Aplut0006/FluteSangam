6f5c7616576c09ce334ac5b5f88bf9142a17a259
Aplut0006 <aplut0006@gmail.com>
2026-09-26 13:44:49 +0530

    feat: add sitemap and sitemap generation script

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -72,7 +72,7 @@ const ROUTE_MOD_DATES: Record<string, string> = {
   '/notations/kal-ho-naa-ho-flute-notes': '2026-09-25',
   '/notations/achyutam-keshavam-flute-notes': '2026-09-21',
   '/notations/radha-krishna-flute-notes': '2026-09-23',
-  '/notations/jana-gana-mana-flute-notes': '2026-09-24',
+  '/notations/jana-gana-mana-flute-notes': '2026-09-26',
   '/privacy-policy': '2026-09-18',
   '/terms-of-service': '2026-09-18',
   '/faq': '2026-09-18',
